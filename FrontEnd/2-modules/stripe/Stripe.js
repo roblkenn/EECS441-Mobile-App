03117788981a6ae0c67fb3a18a01974edbadccbc
carolfes <crolfes@umich.edu>
2019-04-05 15:03:13 -0400

    made values for settings more real

--- FrontEnd/2-modules/stripe/Stripe.js
+++ FrontEnd/2-modules/stripe/Stripe.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
+import { Component } from 'react';
+import stripe from 'tipsi-stripe';
 
-export default function Stripe() {
-  return (
-    <View style={styles.container}>
-      <Text>stripe interface todo</Text>
-    </View>
-  )
+/*stripe.setOptions({
+  publishableKey:'pk_test_6kET7677UpCRMhY20DP7Fbag00l2dxiWry',
+});
+
+export default class Payment extends Component {
+    requestPayment = () => {
+    return stripe
+      .paymentRequestWithCardForm()
+      .then(stripeTokenInfo => {
+        console.warn('Token created', { stripeTokenInfo });
+      })
+      .catch(error => {
+        console.warn('Payment failed', { error });
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Make a payment"
+          onPress={this.requestPayment}
+          disabled={this.state.isPaymentPending}
+        />
+      </View>
+    );
+  }
 }
 
-const styles =  StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+  },
+};
+*/
--- FrontEnd/android/app/src/main/java/com/four/MainApplication.java
+++ FrontEnd/android/app/src/main/java/com/four/MainApplication.java
@@ -3,6 +3,7 @@ package com.four;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.gettipsi.stripe.StripeReactPackage;
 import com.rnfs.RNFSPackage;
 import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new StripeReactPackage(),
             new RNFSPackage(),
             new RNImgToBase64Package(),
             new RNReactNativeHapticFeedbackPackage(),
