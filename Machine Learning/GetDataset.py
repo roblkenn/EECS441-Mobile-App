535820c23651dee469d6a8e15fe01fe721f8d729
Robert Kennedy <robert076kennedy@gmail.com>
2019-04-06 19:02:10 -0400

    - Removed the pods folder - Updated gitignore - Updated image preprocessing script.

--- Machine Learning/GetDataset.py
+++ Machine Learning/GetDataset.py
@@ -3,16 +3,30 @@ from Datum import Datum
 from DatumRepository import DatumRepository
 from ImageRepository import ImageRepository
 from string import Template
+import tensorflow as tf
+import numpy as np
 
 dataset = DatumRepository().read()
 imageRepo = ImageRepository()
+
+x = np.array(len(dataset))
+y = np.array(len(dataset))
 
 for i, item in enumerate(dataset):
 	blobName = item.blobName
 	imageBlob = imageRepo.read(blobName)
 	image = b64decode(imageBlob.content)
 
-	filenameTemplate = Template('$i.jpg')
-	f = open(filenameTemplate.substitute(i=i), 'wb')
-	f.write(image)
+	x[i] = tf.io.decode_jpeg(image, channels=3)
+	y[i] = [item.contrast, item.brightness, item.temperature, item.saturation]
 	
+preprocessor = tf.keras.preprocessing.image.ImageDataGenerator(
+	rotation_range=360,
+	zoom_range=[0.5, 1.5],
+	horizontal_flip=True,
+	vertical_flip=True,
+	validation_split=0.2,
+	shear_range=30
+)
+
+awesomeDataset = preprocessor.flow(x, y)
